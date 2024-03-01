568da597b5b300c895a64f41b68df2fb0331dfe0
Vannara43 <vannarathong@gmail.com>
2024-02-29 18:17:10 -0800

    added printfile.java

---
+++ src/PrintFile.java
@@ -0,0 +1,7 @@
+public class PrintFile {
+    public static void main(String[] args) {
+        System.out.println("This is a print with new line");
+        System.out.print("This is a print without new line");
+        System.out.print("...This is another print on the same line");
+    }
+}
