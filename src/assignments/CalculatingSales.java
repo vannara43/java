eae0497081d14afdcfac2f20d75c4347eb855e4b
Vannara43 <vannarathong@gmail.com>
2024-03-05 19:11:28 -0800

    added CalculatingSales file

---
+++ src/assignments/CalculatingSales.java
@@ -0,0 +1,43 @@
+package assignments;
+
+import java.util.Scanner;
+
+/*
+An online retailer sells five products whose retail
+prices are as follows: 
+Product 1, $2.98; 
+product 2, $4.50; 
+product 3, $9.98;
+product 4, $4.49 and 
+product 5, $6.87. 
+
+Write an application that reads a series of pairs of numbers as follows:
+1. product number
+2. quantity sold
+
+Your program should use a switch statement to determine the retail price
+for each product. It should calculate and display the total retail value of all
+products sold. Use a sentinel-controlled loop to determine when the
+program should stop looping and display the final results
+ */
+public class CalculatingSales {
+    public static void main(String[] args) {
+        /*
+         * IMPORT Scanner Method
+         * DECLARE variable to store total sales
+         * OUTPUT "Enter product number(1-5 or 0 to stop)";
+         * productID = INPUT();
+         * WHILE productId is less than 1 and productID is greater than 5
+         * OUTPUT "Enter quantity sold:"
+         * quantity = INPUT()
+         * 
+         * Switch ProductID {
+         * CASE 1:
+         * CASE 2:
+         * CASE 3:
+         * CASE 4:
+         * CASE 5:
+         * }
+         */
+    }
+}
